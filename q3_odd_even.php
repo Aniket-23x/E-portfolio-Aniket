096d3718ec749785c88beeea979bb066cbb4792b
Aniket choudhary <aniketchoudhary7085@gmail.com>
2026-03-16 13:43:05 +0530

    Update q3_odd_even.php

--- q3_odd_even.php
+++ q3_odd_even.php
@@ -1,8 +1,30 @@
-<?php
-$num = 10;
+//Check whether number is odd or even
 
-if($num % 2 == 0)
-echo "Even";
-else
-echo "Odd";
+
+<html>
+<head>
+    <title>Odd or Even</title>
+</head>
+<body>
+
+<h2>Check Odd or Even</h2>
+
+<form method="post">
+    Enter Number: <input type="number" name="number" required>
+    <input type="submit" value="Check">
+</form>
+
+<?php
+if(isset($_POST['number'])){
+
+    $num = $_POST['number'];
+
+    if($num % 2 == 0)
+        echo "<h3>$num is Even</h3>";
+    else
+        echo "<h3>$num is Odd</h3>";
+}
 ?>
+
+</body>
+</html>

2edd8ec932db6ccabea3b815a13cde9aaf1b7001
Aniket choudhary <aniketchoudhary7085@gmail.com>
2026-03-16 13:32:22 +0530

    Update q1_form_echo.php

--- q1_form_echo.php
+++ q1_form_echo.php
@@ -1,11 +1,25 @@
+// 1. Create a simple HTML form and accept the user name and display the name through PHP echo
+
+
+<html>
+<head>
+    <title>Display Name</title>
+</head>
+<body>
+
+<h2>Enter Your Name</h2>
+
+<form method="post">
+    Name: <input type="text" name="username" required>
+    <input type="submit" value="Submit">
+</form>
+
 <?php
-if(isset($_POST['name'])){
-    $name = $_POST['name'];
-    echo "Hello " . $name;
+if(isset($_POST['username'])) {
+    $name = $_POST['username'];
+    echo "<h3>Hello, " . $name . "!</h3>";
 }
 ?>
 
-<form method="post">
-Name: <input type="text" name="name">
-<input type="submit">
-</form>
+</body>
+</html>

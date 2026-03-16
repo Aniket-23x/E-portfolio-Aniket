03bd0732db8a44f408f1a5d6554bd75114709fd0
Aniket choudhary <aniketchoudhary7085@gmail.com>
2026-03-16 13:49:09 +0530

    Create q5_reverse_1_10.php

---
+++ q5_reverse_1_10.php
@@ -0,0 +1,21 @@
+//  Q5 Print numbers 1–10 in reverse order
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Reverse Numbers</title>
+</head>
+<body>
+
+<h2>Numbers from 10 to 1</h2>
+
+<?php
+
+for($i = 10; $i >= 1; $i--){
+    echo $i . " ";
+}
+
+?>
+
+</body>
+</html>

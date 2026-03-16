7afa858faffd7ad213854f9be9abb0428d3486be
Aniket choudhary <aniketchoudhary7085@gmail.com>
2026-03-16 14:30:34 +0530

    Create q23_copyright.php for copyright display
    Add a PHP script to display copyright information with the current year.

---
+++ q23_copyright.php
@@ -0,0 +1,22 @@
+<?php
+// Question 23: Write a PHP script to display copyright information with the current year.
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Copyright</title>
+</head>
+<body>
+
+<h2>Copyright Information</h2>
+
+<?php
+
+echo "© " . date("Y") . " PHP Exercises";
+
+?>
+
+</body>
+</html>
